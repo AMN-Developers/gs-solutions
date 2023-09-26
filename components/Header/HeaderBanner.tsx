6174eae3af8f23ec540c385190cb49e505dea41d
Felipe Mendes <lfelipecmendes@icloud.com>
2023-09-26 08:35:55 -0300

    feat: header banner

--- components/Header/HeaderBanner.tsx
+++ components/Header/HeaderBanner.tsx
@@ -1,24 +1,33 @@
-import { Container } from '@chakra-ui/react'
-import Typewriter from 'typewriter-effect'
+import { Container, Text } from '@chakra-ui/react'
+import { Typewriter } from 'react-simple-typewriter'
 
 export default function HeaderBanner() {
   return (
     <Container
-      maxW={'container.xl'}
+      maxW={'container.sm'}
       color={'white'}
       centerContent
       py={8}
     >
-      <Typewriter
-        options={{
-          strings: [
-            'G&S Home Solutions',
-            'É a primeira empresa do Brasil com uma linha completa de higienização e impermeabilização de estofados!'
-          ],
-          autoStart: true,
-          loop: true,
-        }}
-      />
+      <Text as='h1' fontWeight={'bold'} fontSize={'2xl'}>
+        G&S HOME SOLUTIONS{' '}
+        <Text as='span' fontWeight={'normal'}>
+          é a primeira empresa do Brasil com uma linha completa de higienização e impermeabilização de
+          <Text as='span' color={'#06EC46'}>
+            {' '}
+            <Typewriter
+              words={['estofados']}
+              loop={true}
+              cursor
+              cursorStyle='_'
+              typeSpeed={70}
+              deleteSpeed={50}
+              delaySpeed={1000}
+              onLoopDone={() => console.log(`Done after 5 loops!`)}
+            />
+          </Text>
+        </Text>
+      </Text>
     </Container>
   )
 }
