8f487b6e042deb74e1529c059df892b7951f6049
Felipe Mendes <lfelipecmendes@icloud.com>
2023-09-29 12:07:29 -0300

    chore: fixed meta tags

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -14,16 +14,16 @@ export default function MyApp({ Component, pageProps }: AppProps) {
       <Head>
         <meta name='viewport' content='minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, user-scalable=no, viewport-fit=cover' />
 
-        <meta name="pwa-demo" content="pwa-demo" />
+        <meta name="pwa-gs" content="pwa-gs" />
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-status-bar-style" content="default" />
-        <meta name="apple-mobile-web-app-title" content="pwa-demo" />
-        <meta name="description" content="pwa-demo" />
+        <meta name="apple-mobile-web-app-title" content="pwa-gs" />
+        <meta name="description" content="pwa-gs" />
         <meta name="format-detection" content="telephone=no" />
         <meta name="mobile-web-app-capable" content="yes" />
-        <meta name="msapplication-TileColor" content="#2B5797" />
+        <meta name="msapplication-TileColor" content="#263869" />
         <meta name="msapplication-tap-highlight" content="no" />
-        <meta name="theme-color" content="#000000" />
+        <meta name="theme-color" content="#263869" />
 
         <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
         <link rel="manifest" href="/manifest.json" />
