eb9de51a10f18f9b2212829a29e8a8735c941e41
Felipe Mendes <lfelipecmendes@icloud.com>
2023-09-29 12:23:39 -0300

    chore: fixed viewport tag

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -12,7 +12,7 @@ export default function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
-        <meta name='viewport' content='minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, user-scalable=no, viewport-fit=cover' />
+        <meta name='viewport' content='minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, viewport-fit=cover' />
 
         <meta name="pwa-gs" content="pwa-gs" />
         <meta name="apple-mobile-web-app-capable" content="yes" />
