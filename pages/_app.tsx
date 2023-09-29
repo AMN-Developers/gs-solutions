1bbc7697611dc3d1eeb7fcd7837a2fc86125dfcf
Felipe Mendes <lfelipecmendes@icloud.com>
2023-09-29 11:49:44 -0300

    fix: fixed manifest

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,11 +5,30 @@ import { AnimatePresence } from "framer-motion"
 import theme from "@/libs/theme"
 import GoogleAnalytics from "@/components/GoogleAnalytics"
 import Layout from "@/components/Layout"
+import Head from 'next/head'
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   const router = useRouter()
   return (
     <>
+      <Head>
+        <meta name='viewport' content='minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, user-scalable=no, viewport-fit=cover' />
+
+        <meta name="pwa-demo" content="pwa-demo" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+        <meta name="apple-mobile-web-app-title" content="pwa-demo" />
+        <meta name="description" content="pwa-demo" />
+        <meta name="format-detection" content="telephone=no" />
+        <meta name="mobile-web-app-capable" content="yes" />
+        <meta name="msapplication-TileColor" content="#2B5797" />
+        <meta name="msapplication-tap-highlight" content="no" />
+        <meta name="theme-color" content="#000000" />
+
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+        <link rel="manifest" href="/manifest.json" />
+        <link rel="shortcut icon" href="/favicon.ico" />
+      </Head>
       <GoogleAnalytics />
       <ChakraProvider theme={theme}>
         <Layout route={router.route}>
