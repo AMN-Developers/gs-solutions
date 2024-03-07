f409cecb411f23d709f649c417afde41fc082fa8
Nino <brunosanctis1997@gmail.com>
2024-03-07 08:52:23 -0300

    feat: page 404

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,20 +1,49 @@
-import MotionLayout from "@/components/MotionLayout";
-import { Container } from "@chakra-ui/react";
+import MotionLayout from "@/components/MotionLayout"
+import { Button, Container, Flex, Text } from "@chakra-ui/react"
+import Image from "next/image"
+import Link from "next/link"
 
 export default function Custom404() {
   return (
-    <MotionLayout title="404: This page could not be found">
+    <MotionLayout title="404: Está pagina não foi encontrada">
       <Container
         as="section"
         maxW={"container.xl"}
         py={4}
         position={"relative"}
       >
-        <div className="flex flex-col items-center justify-center w-full h-full">
-          <h1 className="text-4xl font-bold">404</h1>
-          <p className="text-lg">This page could not be found</p>
-        </div>
+        <Flex
+          flexDir={"column"}
+          justifyContent={"center"}
+          alignItems={"center"}
+          p={10}
+          gap={4}
+          textAlign={"center"}
+        >
+          <Image alt="Imagem 404" src={"/404.png"} width={300} height={300} />
+          <Text as={"p"} fontSize={"2xl"} fontWeight={"bold"}>
+            Pagina não encontrada
+          </Text>
+          <Text
+            as={"p"}
+            fontSize={{ base: "lg", md: "xl" }}
+            fontWeight={"semibold"}
+          >
+            A pagina que está tentando acessar não existe, certifique-se do url
+            foi inserido corretamente
+          </Text>
+          <Button
+            size={{ base: "sm", sm: "md" }}
+            as={Link}
+            mx={"auto"}
+            href={"/"}
+            colorScheme="facebook"
+            _hover={{ color: "facebook.100" }}
+          >
+            Retornar para pagina inicial
+          </Button>
+        </Flex>
       </Container>
     </MotionLayout>
-  );
+  )
 }
