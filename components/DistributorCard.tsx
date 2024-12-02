c3dfe8f1e1b403d72f36892c9b1ddef5d1d6c56e
Felipe Mendes <lfelipecmendes@icloud.com>
2024-12-02 10:18:42 -0300

    feat: add map pin click interactivity

--- components/DistributorCard.tsx
+++ components/DistributorCard.tsx
@@ -3,6 +3,7 @@ import { Flex, Text, Link as ChakraLink } from "@chakra-ui/react";
 import { BsTelephoneFill, BsLink, BsArrow90DegLeft, BsFillPinMapFill } from "react-icons/bs";
 import Link from "next/link";
 import useMapContext from "@/hooks/useMapContext";
+import { forwardRef } from "react";
 
 interface IDistributorCardProps {
   distributor: Distributor;
@@ -10,51 +11,56 @@ interface IDistributorCardProps {
   onSelect: (distributor: Distributor) => void;
 }
 
-export const DistributorCard = ({ distributor, isSelected, onSelect }: IDistributorCardProps) => {
-  const { handleSelectStore } = useMapContext();
+export const DistributorCard = forwardRef<HTMLDivElement, IDistributorCardProps>(
+  ({ distributor, isSelected, onSelect }, ref) => {
+    const { handleSelectStore } = useMapContext();
 
-  const handleClick = () => {
-    onSelect(distributor);
-    handleSelectStore(distributor);
-  };
+    const handleClick = () => {
+      onSelect(distributor);
+      handleSelectStore(distributor);
+    };
 
-  return (
-    <Flex
-      flexDir="column"
-      rounded="md"
-      p={4}
-      mb={2}
-      border="2px solid"
-      borderColor={isSelected ? "blue.500" : "gray.200"}
-      bg={isSelected ? "blue.50" : "white"}
-      _hover={{ cursor: "pointer", borderColor: "blue.500" }}
-      w="full"
-      onClick={handleClick}
-    >
-      <Flex alignItems="center">
-        <Text fontWeight="bold">{distributor.name}</Text>
-        {distributor.distance && (
-          <Flex alignItems="center" ml={2} color="blue.500">
-            <Text mr={2}>{distributor.distance.toFixed(2)} km</Text>
-            <BsFillPinMapFill />
+    return (
+      <Flex
+        ref={isSelected ? ref : undefined}
+        flexDir="column"
+        rounded="md"
+        p={4}
+        mb={2}
+        border="2px solid"
+        borderColor={isSelected ? "blue.500" : "gray.200"}
+        bg={isSelected ? "blue.50" : "white"}
+        _hover={{ cursor: "pointer", borderColor: "blue.500" }}
+        w="full"
+        onClick={handleClick}
+      >
+        <Flex alignItems="center">
+          <Text fontWeight="bold">{distributor.name}</Text>
+          {distributor.distance && (
+            <Flex alignItems="center" ml={2} color="blue.500">
+              <Text mr={2}>{distributor.distance.toFixed(2)} km</Text>
+              <BsFillPinMapFill />
+            </Flex>
+          )}
+        </Flex>
+        <Text color="gray.500">{distributor.address}</Text>
+        {distributor.phone && (
+          <Flex alignItems="center" mt={2}>
+            <BsTelephoneFill />
+            <Text ml={2}>{distributor.phone}</Text>
+          </Flex>
+        )}
+        {distributor.website && (
+          <Flex alignItems="center" mt={2} color="blue.500">
+            <BsLink />
+            <ChakraLink as={Link} href={distributor.website} ml={2} target="_blank">
+              {distributor.website}
+            </ChakraLink>
           </Flex>
         )}
       </Flex>
-      <Text color="gray.500">{distributor.address}</Text>
-      {distributor.phone && (
-        <Flex alignItems="center" mt={2}>
-          <BsTelephoneFill />
-          <Text ml={2}>{distributor.phone}</Text>
-        </Flex>
-      )}
-      {distributor.website && (
-        <Flex alignItems="center" mt={2} color="blue.500">
-          <BsLink />
-          <ChakraLink as={Link} href={distributor.website} ml={2} target="_blank">
-            {distributor.website}
-          </ChakraLink>
-        </Flex>
-      )}
-    </Flex>
-  );
-};
+    );
+  }
+);
+
+DistributorCard.displayName = "DistributorCard";
--- components/DistributorsMap.tsx
+++ components/DistributorsMap.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useRef } from "react";
 import { Flex } from "@chakra-ui/react";
 import { DistributorCard } from "./DistributorCard";
 import { Distributor } from "@/context/DISTRIBUTORS_ITEMS";
@@ -9,11 +10,23 @@ interface IDistributorMapProps {
 }
 
 export const DistributorsMap = ({ distributors, selectedDistributor, onSelect }: IDistributorMapProps) => {
+  const selectedCardRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    if (selectedDistributor && selectedCardRef.current) {
+      selectedCardRef.current.scrollIntoView({
+        behavior: "smooth",
+        block: "center",
+      });
+    }
+  }, [selectedDistributor]);
+
   return (
-    <Flex flexDir={"column"} overflowY={"auto"} w={"100%"}>
+    <Flex flexDir="column" overflowY="auto" w="100%">
       {distributors.map((store) => (
         <DistributorCard
           key={store.id}
+          ref={selectedDistributor?.id === store.id ? selectedCardRef : undefined}
           distributor={store}
           isSelected={selectedDistributor?.id === store.id}
           onSelect={onSelect}
