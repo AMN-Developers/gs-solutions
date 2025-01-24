1b5e7c52c4991f69d0b904e7656e5ada2b132f2f
Felipe Mendes <lfelipecmendes@icloud.com>
2025-01-24 16:06:06 -0300

    fix: add react platyer dependency

--- components/iFrame.tsx
+++ components/iFrame.tsx
@@ -1,6 +1,6 @@
-import { AspectRatio, Box } from "@chakra-ui/react";
-import { useState } from "react";
-import Image from "next/image";
+import dynamic from "next/dynamic";
+import { AspectRatio } from "@chakra-ui/react";
+const ReactPlayerNoSSR = dynamic(() => import("react-player"), { ssr: false });
 
 interface VideoFrameProps {
   embedID: string;
@@ -13,48 +13,13 @@ export default function VideoFrame({
   title,
   ariaDescription,
 }: VideoFrameProps) {
-  const [isImageClicked, setIsImageClicked] = useState(false);
-
   return (
     <AspectRatio ratio={16 / 9}>
-      {!isImageClicked ? (
-        <Box
-          as="button"
-          onClick={() => setIsImageClicked(true)}
-          position="relative"
-          width="100%"
-          height="100%"
-        >
-          <Box
-            as={Image}
-            src={`https://img.youtube.com/vi/${embedID}/sddefault.jpg`}
-            layout="fill"
-            objectFit="cover"
-            alt="yt thumbnail"
-            priority
-            rounded={"md"}
-          />
-          <Box position={"absolute"}>
-            <Image
-              src={"/yt-playbutton.svg"}
-              alt="play button"
-              width={50}
-              height={50}
-            />
-          </Box>
-        </Box>
-      ) : (
-        <Box
-          as={"iframe"}
-          title={title}
-          src={`https://www.youtube.com/embed/${embedID}?controls=1&modestbranding=1&rel=0&showinfo=1&color=white&iv_load_policy=3&fs=1&disablekb=1&autoplay=1`}
-          loading="lazy"
-          // eslint-disable-next-line jsx-a11y/aria-props
-          aria-description={ariaDescription}
-          rounded={"md"}
-          allowFullScreen
-        />
-      )}
+      <ReactPlayerNoSSR
+        url={`https://www.youtube.com/watch?v=${embedID}`}
+        title={title}
+        ariaDescription={ariaDescription}
+      />
     </AspectRatio>
   );
 }
